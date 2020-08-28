34cec893d2f8b5721d7c592241257d6f25835513
CHI-HUI CHOU <afai97202013@gmail.com>
2020-08-28 15:45:18 +0800

    feat: add forms and some models

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # demo-youtube-clone
 Youtube Clone
+
+## Features
+
+* Upload your videos
+
+
+## Stacks
+
+* Flask
+* Flask-SQLAlchemy
+* Flask-Migrate
+* Flask-WTF
+* Flask-Login
+* SQLite (dev-db)
+* PostgreSQL(pro-db)
+
+
+## 
+
+
+## For Development Only
+
+### Database Schema
+TODO
+
+### Rest API
+
+TODO
+
+
+## For Deployment Only
+
+### Server
+TODO
+
+
+
+### Container
+TODO
+
--- app.py
+++ app.py
@@ -1,23 +1,162 @@
 import os
 from flask import Flask, render_template, redirect, url_for, send_from_directory, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_required, current_user, logout_user, login_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileField
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+from datetime import datetime
+
 
 basedir = os.path.abspath(os.path.dirname(__name__))
 app = Flask(__name__)
 
+
+# config
 app.config['SECRET_KEY'] = 'onlyfortest'
 app.config['UPLOAD_DIR'] = os.path.join(basedir, 'uploads')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# extensions
+db = SQLAlchemy(app)
+login_manger = LoginManager(app)
+migrate = Migrate(app, db)
+
 
 # models
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    username = db.Column(db.String(25), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    profile_path = db.Column(db.String(255))
 
 
+@login_manger.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Video(db.Model):
+    __tablename__ = 'videos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(70), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    is_private = db.Column(db.Boolean, default=False)
+    file_path = db.Column(db.String(250),nullable=False)
+    
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
 
 
 # forms
+class VideoForm(FlaskForm):
+    video = FileField('Your video', validators=[
+        DataRequired()
+    ])
+
+    title = StringField('Title', validators=[
+        DataRequired(),
+        Length(max=70),
+    ])
+    description = TextAreaField('Description', validators=[
+        DataRequired(),
+        Length(max=1000),
+    ])
+    is_private = SelectField('Privacy', coerce=bool, choices=[
+        (True, 'Private'),
+        (False, 'Public'),
+    ])
+    category = SelectField('Category', coerce=int)
 
 
+class SignupForm(FlaskForm):
+    first_name = StringField('First Name', validators=[
+        DataRequired(),
+        Length(max=25)
+    ])
+
+    last_name = StringField('Last Name', validators=[
+        DataRequired(),
+        Length(max=25)
+    ])
+
+    username = StringField('Username', validators=[
+        DataRequired(),
+        Length(max=25)
+    ])
+
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Length(max=100),
+        Email(),
+        EqualTo('email2', message='Emails must match.')
+    ])
+    email2 = StringField('Confirm email')
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(8, 16),
+        EqualTo('password2', message='Passwords must match')
+    ])
+    password2 = PasswordField('Confirm Password')
+
+
+class SigninForm(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(),
+    ])
+
+    password = PasswordField('Password', validators=[
+        DataRequired()
+    ])
+
+
+class UserUpdateForm(FlaskForm):
+    first_name = StringField('First Name', validators=[
+        DataRequired(),
+        Length(max=25)
+    ])
+    last_name = StringField('Last Name', validators=[
+        DataRequired(),
+        Length(max=25)
+    ])
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Length(max=100),
+        Email()
+    ])
+
+
+class PasswordUpdateForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[
+        DataRequired()
+    ])
+    password = PasswordField('New Password', validators=[
+        DataRequired(),
+        Length(8, 16),
+        EqualTo('password2', message='Passwords must match')
+    ])
+    password2 = PasswordField('Confirm new password', validators=[
+        DataRequired(),
+    ])
 
 
 @app.route('/')
@@ -27,7 +166,31 @@ def index():
 
 @app.route('/upload')
 def upload():
-    return render_template('upload.html')
+
+    form = VideoForm()
+    
+    # only for test
+    category = [
+        (1, 'Film & Animation'),
+        (2, 'Autos & Vehicles'),
+        (3, 'Music'),
+        (4, 'Pets & Animals'),
+        (5, 'Sports'),
+        (6, 'Travel & Events'),
+        (7, 'Gaming'),
+        (8, 'People & Blogs'),
+        (9, 'Comedy'),
+        (10, 'Entertainment'),
+        (11, 'News & Politics'),
+        (12, 'Howto & Style'),
+        (13, 'Education'),
+        (14, 'Science & Technology'),
+        (15, 'Nonprofits & Activism')
+    ]
+    form.category.choices = category
+
+
+    return render_template('upload.html', form=form)
 
 
 @app.route('/search')
@@ -42,12 +205,16 @@ def profile():
 
 @app.route('/signin')
 def signin():
-    return render_template('signin.html')
+    form = SigninForm()
+
+    return render_template('signin.html', form=form)
 
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    form = SignupForm()
+
+    return render_template('signup.html', form=form)
 
 @app.route('/logout')
 def logout():
@@ -56,7 +223,14 @@ def logout():
 
 @app.route('/settings')
 def settings():
-    return render_template('settings.html')
+    userForm = UserUpdateForm()
+    passwordForm = PasswordUpdateForm()
+
+
+    return render_template('settings.html', 
+        userForm=userForm, 
+        passwordForm=passwordForm
+    )
 
 
 @app.route('/subscriptions')
