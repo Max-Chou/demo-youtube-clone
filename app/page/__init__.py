5c30cdd9eb3083ad52c4b5f83e956cc0d9d7a8ac
CHI-HUI CHOU <afai97202013@gmail.com>
2020-09-25 22:48:07 +0800

    feat: init commit

--- app/page/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-page = Blueprint('page', __name__)
-
-from . import views
--- app/utils.py
+++
@@ -1,5 +0,0 @@
-
-
-
-def allowed_file(filename, allowed_format):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_format
